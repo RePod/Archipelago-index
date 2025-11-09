7c6d108ec0d4f1330864151c8f625c683022e17e
Bastien Orivel <eijebong@bananium.fr>
2025-11-09 18:25:16 +0100

    Properly override the optimization schema for tasks
    https://github.com/taskcluster/taskgraph/commit/c60445965e81d58a78bd6ad01e406820c2e28f66
    changed the order things are getting import in which means we do not get
    a chance to override OptimizationSchema before the Task schema gets
    compiled. So we now override it manually and recompile the schema. It's
    not pretty, but as far as I know we don't really have a choice if we
    want to continue using the task transform.

--- taskcluster/src/optimize/schema.py
+++ taskcluster/src/optimize/schema.py
@@ -1,5 +1,6 @@
 import voluptuous
 import taskgraph.util.schema
+from taskgraph.transforms.task import task_description_schema
 
 taskgraph.util.schema.OptimizationSchema = voluptuous.Any(
     # always run this task (default)
@@ -12,4 +13,8 @@ taskgraph.util.schema.OptimizationSchema = voluptuous.Any(
 
     {"skip-unless-changed-or-cached": {"skip-unless-changed": [str], "index-path": [str]}},
 )
-
+# XXX: Ugly hack because the task schema gets compiled before we get a chance to override the OptimizationSchema
+# As far as I know there's no other way to change the optimization schema at the moment. And since taskgraph will get rid
+# of voluptuous soon™ I don't want to spend too much time on this. It works, it's good enough
+task_description_schema.schema["optimization"] = taskgraph.util.schema.OptimizationSchema
+task_description_schema._compiled = task_description_schema._compile(task_description_schema.schema)
