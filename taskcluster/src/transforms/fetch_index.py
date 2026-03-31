3bf9dd185ce15105218bc6d9110ba978c3f2a7b1
Bastien Orivel <eijebong@bananium.fr>
2026-03-31 23:31:12 +0200

    Rework how fetch optimizations work to be able to pull main ones from PRs (#1683)

--- taskcluster/src/transforms/fetch_index.py
+++ taskcluster/src/transforms/fetch_index.py
@@ -19,18 +19,24 @@ def fetch_index(config, tasks):
         safe_name = _sanitize_index_path(apworld_name)
         safe_version = _sanitize_index_path(version)
 
-        if is_main:
-            index_path = f"ap.{project}.world.{safe_name}.{safe_version}"
-        else:
-            index_path = f"ap.{project}.try.world.{safe_name}.{safe_version}"
+        main_index_path = f"ap.{project}.world.{safe_name}.{safe_version}"
 
-        task.setdefault("routes", []).append(f"index.{index_path}")
+        if is_main:
+            task.setdefault("routes", []).append(f"index.{main_index_path}")
+            extra_index = task.setdefault("extra", {}).setdefault("index", {})
+            extra_index["rank"] = "build_date"
+            optimization_paths = [main_index_path]
+        elif config.params.get('try_config'):
+            try_index_path = f"ap.{project}.try.world.{safe_name}.{safe_version}"
+            task.setdefault("routes", []).append(f"index.{try_index_path}")
+            extra_index = task.setdefault("extra", {}).setdefault("index", {})
+            extra_index["rank"] = "build_date"
+            optimization_paths = [try_index_path, main_index_path]
+        else:
+            optimization_paths = [main_index_path]
 
         task["optimization"] = {
-            "index-search": [index_path],
+            "index-search": optimization_paths,
         }
-
-        extra_index = task.setdefault("extra", {}).setdefault("index", {})
-        extra_index["rank"] = "build_date"
 
         yield task
