d3fe80ddb20e7d7c136eb9d3692c9fca7f65e2f2
Bastien Orivel <eijebong@bananium.fr>
2026-04-03 00:48:17 +0200

    Remove workaround for taskcluster replacing spaces with + (#1693)

--- taskcluster/src/transforms/make_expectations_patch.py
+++ taskcluster/src/transforms/make_expectations_patch.py
@@ -12,7 +12,6 @@ def add_all_expectations_deps(config, tasks):
         attributes = task.setdefault("attributes", {})
 
         soft_fetches = attributes.setdefault("soft-fetches", {})
-        # Something replaces spaces in artifact names with a `+`
-        soft_fetches.update({dep.label: { "artifact": "public/test_results/{}.toml".format(dep.attributes['apworld_name'].replace(" ", "+")), "dest": "/builds/worker/checkouts/vcs/meta/"} for dep in deps})
+        soft_fetches.update({dep.label: { "artifact": "public/test_results/{}.toml".format(dep.attributes['apworld_name']), "dest": "/builds/worker/checkouts/vcs/meta/"} for dep in deps})
         yield task
 
